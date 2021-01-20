89270dd22bf3a5cf63fb2806367dbcdf7f66d679
jaredgoodin <813552@mystma.org>
2021-01-20 19:14:50 +0000

    sumBetween try #1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def sumBetween(x,y):
+   
+    dog = 0 
+   
+    for i in range (x + 1,y):
+        dog = dog + i 
+        
+    
+    return dog
+
+print (sumBetween (5,7))
